eac0dae34360a93f477b12819f061f7bf8f4e014
moncheeta <moncheeta@duck.com>
2024-01-30 07:37:15 -0600

    Initial Commit

---
+++ Question-Of-The-Day/Question_Of_The_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Question_Of_The_DayApp.swift
+//  Question-Of-The-Day
+//
+//  Created by Damian Myrda on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Question_Of_The_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
