35d6a454701aef0a6d225beda1eeac01e8b8c3d2
moncheeta <moncheeta@duck.com>
2024-02-09 10:52:22 -0600

    Remove some padding

--- App.swift
+++ App.swift
@@ -58,7 +58,6 @@ struct Question_Of_The_Day: App {
                     LinearGradient(colors: [.cyan, .green], startPoint: .topLeading, endPoint: .bottomTrailing)
                         .ignoresSafeArea()
                     VStack {
-                        Spacer()
                         login
                             .background(ignoresSafeAreaEdges: .all)
                         Button(action: { () -> Void in
@@ -74,7 +73,6 @@ struct Question_Of_The_Day: App {
                                 await self.login()
                             }
                         }
-                        Spacer()
                     }
                 }
             case .question(var attempt):
--- Login.swift
+++ Login.swift
@@ -6,35 +6,22 @@ struct Login: View {
     @State var name: String = ""
     var body: some View {
         ZStack {
-            
             LinearGradient(colors: [.cyan, .green], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
-            
             VStack {
                 Text("Welcome to the Question of the Day!")
                     .font(.system(.title, design: .rounded))
-                                    .fontWeight(.heavy)
                     .fontWeight(.bold)
                     .foregroundColor(.white)
                     .padding(.bottom, 20)
                 
                 TextField("Your Name", text: self.$name)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                .padding()
-                            
-                
-                Button(action: {
-                    
-                }) {
-                   
-                }
-                .padding(.top, 20)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
             }
-            .padding()
-            .background(Color.gray.opacity(0.1))
             .cornerRadius(15)
+            .background(Color.gray.opacity(0.1))
             .padding(.horizontal, 20)
         }
     }
-    
 }
