908e8d203cb3447648aea757851fd8c0cd62b7a1
moncheeta <moncheeta@duck.com>
2024-02-15 10:44:45 -0600

    Change spacing in login view

--- Login.swift
+++ Login.swift
@@ -5,18 +5,16 @@ struct Login: View {
     
     @State var name: String = ""
     var body: some View {
-        VStack {
+        VStack(spacing: 30) {
             Text("Welcome to the Question of the Day!")
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .foregroundColor(.white)
-                .padding(.bottom, 30)
             TextField("Your Name", text: self.$name)
                 .onChange(of: self.name) {
                     self.player.name = self.name
                 }
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.bottom, 30)
         }
         .padding()
     }
